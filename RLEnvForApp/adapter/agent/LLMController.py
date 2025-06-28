6b758c313a34424dcd1f50677762c7ebc7f1da6f
Chris Chen <chrischen0618@gmail.com>
2025-06-28 22:32:53 +0800

    Mark useless codes as comments
    Signed-off-by: Chris Chen <chrischen0618@gmail.com>

--- RLEnvForApp/adapter/agent/LLMController.py
+++ RLEnvForApp/adapter/agent/LLMController.py
@@ -185,146 +185,146 @@ class LLMController:
             end_time = time.time_ns()
             print(f"Total time: {(end_time - begin_time) / 1000000}ms")
 
-    def _check_is_password(self, app_element: AppElement):
-        # check if the element is a password field use regex
-        if re.search(r'password', app_element.getName(), re.IGNORECASE) or re.search(r'password', app_element.getLabel(),
-                                                                                   re.IGNORECASE) or re.search(
-                r'password', app_element.getPlaceholder(), re.IGNORECASE):
-            return True
-        return False
+    # def _check_is_password(self, app_element: AppElement):
+    #     # check if the element is a password field use regex
+    #     if re.search(r'password', app_element.getName(), re.IGNORECASE) or re.search(r'password', app_element.getLabel(),
+    #                                                                                re.IGNORECASE) or re.search(
+    #             r'password', app_element.getPlaceholder(), re.IGNORECASE):
+    #         return True
+    #     return False
 
-    def _save_target_page_to_html_set(self, episode_handler_id: str, directive_dto: DirectiveDTO):
-        file_name = f"{self.__server_name}_{urlparse(directive_dto.getUrl()).path.replace('/', '_')}_{directive_dto.getFormXPath().replace('/', '_')}"
-        initial_state_dto: StateDTO = self._get_episode_handler_dto(
-            episode_handler_id=episode_handler_id).getStateDTOs()[0]
+    # def _save_target_page_to_html_set(self, episode_handler_id: str, directive_dto: DirectiveDTO):
+    #     file_name = f"{self.__server_name}_{urlparse(directive_dto.getUrl()).path.replace('/', '_')}_{directive_dto.getFormXPath().replace('/', '_')}"
+    #     initial_state_dto: StateDTO = self._get_episode_handler_dto(
+    #         episode_handler_id=episode_handler_id).getStateDTOs()[0]
 
-        interactive_app_element_dictionary = []
-        directive_dictionary = {}
-        for app_event_dto in directive_dto.getAppEventDTOs():
-            directive_dictionary[app_event_dto.getXpath()] = {
-                "value": app_event_dto.getValue(), "category": app_event_dto.getCategory()}
-        for app_element_dto in initial_state_dto.getSelectedAppElementDTOs():
-            interactive_app_element_dictionary.append(app_element_dto.getXpath())
-        form_x_path = directive_dto.getFormXPath()
-        directive_log_json = json.dumps({"interactive_appElement": interactive_app_element_dictionary,
-                                         "appEvent": directive_dictionary, "formXPath": form_x_path})
+    #     interactive_app_element_dictionary = []
+    #     directive_dictionary = {}
+    #     for app_event_dto in directive_dto.getAppEventDTOs():
+    #         directive_dictionary[app_event_dto.getXpath()] = {
+    #             "value": app_event_dto.getValue(), "category": app_event_dto.getCategory()}
+    #     for app_element_dto in initial_state_dto.getSelectedAppElementDTOs():
+    #         interactive_app_element_dictionary.append(app_element_dto.getXpath())
+    #     form_x_path = directive_dto.getFormXPath()
+    #     directive_log_json = json.dumps({"interactive_appElement": interactive_app_element_dictionary,
+    #                                      "appEvent": directive_dictionary, "formXPath": form_x_path})
 
-        # self._updateInputValueWeights(directiveDictionary)
+    #     # self._updateInputValueWeights(directiveDictionary)
 
-        Logger().info(f"Save html set:\n{file_name}\n{form_x_path}\n{directive_dictionary}")
+    #     Logger().info(f"Save html set:\n{file_name}\n{form_x_path}\n{directive_dictionary}")
 
-        file_manager = FileManager()
-        file_manager.createFolder("htmlSet", "FAILED_HTML_SET")
-        file_manager.createFile(path=os.path.join("htmlSet", "FAILED_HTML_SET"),
-                                fileName=file_name + ".html", context=directive_dto.getDom())
-        file_manager.createFile(path=os.path.join("htmlSet", "FAILED_HTML_SET"),
-                                fileName=file_name + ".json", context=directive_log_json)
+    #     file_manager = FileManager()
+    #     file_manager.createFolder("htmlSet", "FAILED_HTML_SET")
+    #     file_manager.createFile(path=os.path.join("htmlSet", "FAILED_HTML_SET"),
+    #                             fileName=file_name + ".html", context=directive_dto.getDom())
+    #     file_manager.createFile(path=os.path.join("htmlSet", "FAILED_HTML_SET"),
+    #                             fileName=file_name + ".json", context=directive_log_json)
 
-    def _create_directive(self, target_page_id: str, episode_handler_id: str, high_level_action_list: HighLevelActionList):
-        create_directive_use_case = CreateDirectiveUseCase()
-        create_directive_input = CreateDirectiveInput(targetPageId=target_page_id, episodeHandlerId=episode_handler_id, highLevelActionList=high_level_action_list)
-        create_directive_output = CreateDirectiveOutput()
-        create_directive_use_case.execute(create_directive_input, create_directive_output)
-        return create_directive_output.getDirectiveDTO()
+    # def _create_directive(self, target_page_id: str, episode_handler_id: str, high_level_action_list: HighLevelActionList):
+    #     create_directive_use_case = CreateDirectiveUseCase()
+    #     create_directive_input = CreateDirectiveInput(targetPageId=target_page_id, episodeHandlerId=episode_handler_id, highLevelActionList=high_level_action_list)
+    #     create_directive_output = CreateDirectiveOutput()
+    #     create_directive_use_case.execute(create_directive_input, create_directive_output)
+    #     return create_directive_output.getDirectiveDTO()
 
-    def _get_episode_handler_dto(self, episode_handler_id: str) -> EpisodeHandlerDTO:
-        use_case = GetEpisodeHandlerUseCase()
-        _input = GetEpisodeHandlerInput(episodeHandlerId=episode_handler_id)
-        _output = GetEpisodeHandlerOutput()
-        use_case.execute(_input, _output)
-        return _output.getEpisodeHandlerDTO()
+    # def _get_episode_handler_dto(self, episode_handler_id: str) -> EpisodeHandlerDTO:
+    #     use_case = GetEpisodeHandlerUseCase()
+    #     _input = GetEpisodeHandlerInput(episodeHandlerId=episode_handler_id)
+    #     _output = GetEpisodeHandlerOutput()
+    #     use_case.execute(_input, _output)
+    #     return _output.getEpisodeHandlerDTO()
 
-    def _execute_action(self, app_element: AppElement, target_url: str, form_xpath: str) -> ExecuteActionOutput:
-        final_submit = False
-        episode_handler_entity = self._episode_handler_repository.findById(self._episode_handler_id)
-        episode_handler = EpisodeHandlerEntityMapper.mappingEpisodeHandlerForm(episode_handler_entity)
-        states = episode_handler.getAllState()
-        xpath = app_element.getXpath()
+    # def _execute_action(self, app_element: AppElement, target_url: str, form_xpath: str) -> ExecuteActionOutput:
+    #     final_submit = False
+    #     episode_handler_entity = self._episode_handler_repository.findById(self._episode_handler_id)
+    #     episode_handler = EpisodeHandlerEntityMapper.mappingEpisodeHandlerForm(episode_handler_entity)
+    #     states = episode_handler.getAllState()
+    #     xpath = app_element.getXpath()
 
-        # get input values
-        high_level_action: HighLevelAction = self._inputValueHandler.get(target_url, form_xpath)
-        if high_level_action is None:
-            # FInish testing this form
-            execute_action_output = ExecuteActionOutput()
-            execute_action_output.setIsDone(True)
-            return execute_action_output
+    #     # get input values
+    #     high_level_action: HighLevelAction = self._inputValueHandler.get(target_url, form_xpath)
+    #     if high_level_action is None:
+    #         # FInish testing this form
+    #         execute_action_output = ExecuteActionOutput()
+    #         execute_action_output.setIsDone(True)
+    #         return execute_action_output
 
-        execute_action_use_case = ExecuteActionUseCase(self.__aut_operator)
-        doc = etree.parse(StringIO(states[-1].getDOM()), etree.HTMLParser())
-        # find the submit button by xpath
-        app_element_by_xpath = doc.xpath(app_element.getXpath())[0]
-        str1 = 'The Form element:\n' + etree.tostring(doc.xpath(self.__target_form_xpath)[0], pretty_print=True, method="html", encoding="unicode") + '\nThe target element:\n' + etree.tostring(app_element_by_xpath, pretty_print=True, method="html", encoding="unicode")
-        is_submit_button = False
+    #     execute_action_use_case = ExecuteActionUseCase(self.__aut_operator)
+    #     doc = etree.parse(StringIO(states[-1].getDOM()), etree.HTMLParser())
+    #     # find the submit button by xpath
+    #     app_element_by_xpath = doc.xpath(app_element.getXpath())[0]
+    #     str1 = 'The Form element:\n' + etree.tostring(doc.xpath(self.__target_form_xpath)[0], pretty_print=True, method="html", encoding="unicode") + '\nThe target element:\n' + etree.tostring(app_element_by_xpath, pretty_print=True, method="html", encoding="unicode")
+    #     is_submit_button = False
 
-        prompt = SystemPromptFactory.get("is_submit_button").format(form_info=str1)
-        is_submit_button_str = self._llm_service.get_response(prompt).lower()
-        if "yes" in is_submit_button_str:
-            is_submit_button = True
+    #     prompt = SystemPromptFactory.get("is_submit_button").format(form_info=str1)
+    #     is_submit_button_str = self._llm_service.get_response(prompt).lower()
+    #     if "yes" in is_submit_button_str:
+    #         is_submit_button = True
 
-        # if app_element.getTagName() == "button" or app_element.getTagName() == "a" or (app_element.getTagName() == 'input' and (app_element.getType() == 'submit' or app_element.getType() == "button" or app_element.getType() == 'image')):
-        #     is_submit_button = True
+    #     # if app_element.getTagName() == "button" or app_element.getTagName() == "a" or (app_element.getTagName() == 'input' and (app_element.getType() == 'submit' or app_element.getType() == "button" or app_element.getType() == 'image')):
+    #     #     is_submit_button = True
 
-        execute_action_output = ExecuteActionOutput()
+    #     execute_action_output = ExecuteActionOutput()
 
-        if is_submit_button:
-            category = 0
-            final_submit = True
-            app_event = AppEvent("", "", 0)
-        else:
-            repeat_counter = 0
-            app_event: AppEvent = high_level_action.getAppEventByXpath(xpath)
-            while app_event is None:
-                if repeat_counter >= 3:
-                    raise ValueError("Form input value is None for 3 times.")
-                repeat_counter += 1
-                # Get new state
-                state = self.__aut_operator.getState()
-                dom = state.getDOM()
-                # Update input values
-                new_high_level_action_list: HighLevelActionList = InputUpdaterHandler(llm_service=Gemini()).get_response(dom=dom, input_values=high_level_action.toString(), form_xpath=form_xpath, lacked_field_xpath=xpath)
-                if new_high_level_action_list.is_done() == False:
-                    high_level_action = new_high_level_action_list.get()
-                    high_level_action.update(dom, high_level_action.getAppEventDict())
-                    app_event: AppEvent = high_level_action.getAppEventByXpath(xpath)
-            category = app_event.getCategory()
+    #     if is_submit_button:
+    #         category = 0
+    #         final_submit = True
+    #         app_event = AppEvent("", "", 0)
+    #     else:
+    #         repeat_counter = 0
+    #         app_event: AppEvent = high_level_action.getAppEventByXpath(xpath)
+    #         while app_event is None:
+    #             if repeat_counter >= 3:
+    #                 raise ValueError("Form input value is None for 3 times.")
+    #             repeat_counter += 1
+    #             # Get new state
+    #             state = self.__aut_operator.getState()
+    #             dom = state.getDOM()
+    #             # Update input values
+    #             new_high_level_action_list: HighLevelActionList = InputUpdaterHandler(llm_service=Gemini()).get_response(dom=dom, input_values=high_level_action.toString(), form_xpath=form_xpath, lacked_field_xpath=xpath)
+    #             if new_high_level_action_list.is_done() == False:
+    #                 high_level_action = new_high_level_action_list.get()
+    #                 high_level_action.update(dom, high_level_action.getAppEventDict())
+    #                 app_event: AppEvent = high_level_action.getAppEventByXpath(xpath)
+    #         category = app_event.getCategory()
 
-        execute_action_input = ExecuteActionInput(category, self._episode_handler_id, self.__server_name, target_url,
-                                                  app_element.getXpath(), value=app_event.getValue())
+    #     execute_action_input = ExecuteActionInput(category, self._episode_handler_id, self.__server_name, target_url,
+    #                                               app_element.getXpath(), value=app_event.getValue())
 
-        try:
-            execute_action_use_case.execute(input=execute_action_input, output=execute_action_output)
-            episode_handler_entity = self._episode_handler_repository.findById(self._episode_handler_id)
-            episode_handler = EpisodeHandlerEntityMapper.mappingEpisodeHandlerForm(episode_handler_entity)
-            state: State = episode_handler.getAllState()[-2]
-        except Exception as exception:
-            self._logger.exception(f"Something wrong when execute action: {exception}")
-            traceback.print_exc()
-            execute_action_output.setIsDone(True)
-        finally:
-            if final_submit:
-                execute_action_output.setIsDone(True)
-                self._inputValueHandler.next(target_url, form_xpath)
-        return execute_action_output
+    #     try:
+    #         execute_action_use_case.execute(input=execute_action_input, output=execute_action_output)
+    #         episode_handler_entity = self._episode_handler_repository.findById(self._episode_handler_id)
+    #         episode_handler = EpisodeHandlerEntityMapper.mappingEpisodeHandlerForm(episode_handler_entity)
+    #         state: State = episode_handler.getAllState()[-2]
+    #     except Exception as exception:
+    #         self._logger.exception(f"Something wrong when execute action: {exception}")
+    #         traceback.print_exc()
+    #         execute_action_output.setIsDone(True)
+    #     finally:
+    #         if final_submit:
+    #             execute_action_output.setIsDone(True)
+    #             self._inputValueHandler.next(target_url, form_xpath)
+    #     return execute_action_output
 
-    def _is_legal_directive(self):
-        episode_handler_entity = self._episode_handler_repository.findById(self._episode_handler_id)
-        episode_handler = EpisodeHandlerEntityMapper.mappingEpisodeHandlerForm(episode_handler_entity)
-        states = episode_handler.getAllState()
-        # When the length of states is less than 2, it means that the current state is the first state
-        # or the app element is none and then final submit and the is_legal directive is false in this case
-        # episode_handler.remain_only_index_zero_state() will remove states so that the length of states is less than 2
-        if len(states) < 2:
-            return False
-        if states[-2].getActionType() == "click" and states[-2].getInteractedElement():
-            interactive_app_element: AppElement = states[-2].getInteractedElement()
-            tag_name = interactive_app_element.getTagName()
-            tag_type = interactive_app_element.getType()
-            if tag_name == "button" or tag_name == "a" or (tag_name == 'input' and (
-                    tag_type == 'submit' or tag_type == "button" or tag_type == 'image')):
-                after_action_dom = states[-1].getDOM()
-                before_action_dom = states[-2].getDOM()
-                return self._directive_rule_service.isLegal(self._target_page_id, before_action_dom, after_action_dom)
-        return False
+    # def _is_legal_directive(self):
+    #     episode_handler_entity = self._episode_handler_repository.findById(self._episode_handler_id)
+    #     episode_handler = EpisodeHandlerEntityMapper.mappingEpisodeHandlerForm(episode_handler_entity)
+    #     states = episode_handler.getAllState()
+    #     # When the length of states is less than 2, it means that the current state is the first state
+    #     # or the app element is none and then final submit and the is_legal directive is false in this case
+    #     # episode_handler.remain_only_index_zero_state() will remove states so that the length of states is less than 2
+    #     if len(states) < 2:
+    #         return False
+    #     if states[-2].getActionType() == "click" and states[-2].getInteractedElement():
+    #         interactive_app_element: AppElement = states[-2].getInteractedElement()
+    #         tag_name = interactive_app_element.getTagName()
+    #         tag_type = interactive_app_element.getType()
+    #         if tag_name == "button" or tag_name == "a" or (tag_name == 'input' and (
+    #                 tag_type == 'submit' or tag_type == "button" or tag_type == 'image')):
+    #             after_action_dom = states[-1].getDOM()
+    #             before_action_dom = states[-2].getDOM()
+    #             return self._directive_rule_service.isLegal(self._target_page_id, before_action_dom, after_action_dom)
+    #     return False
 
     def _remove_target_page(self):
         remove_target_page_use_case = RemoveTargetPageUseCase()
