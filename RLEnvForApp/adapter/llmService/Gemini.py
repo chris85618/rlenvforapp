69d324a57940d30714bea93caf7a5a1a056cd661
Chris Chen <chrischen0618@gmail.com>
2025-07-13 09:02:03 +0800

    Improve the parameters of Gemini
    Signed-off-by: Chris Chen <chrischen0618@gmail.com>

--- RLEnvForApp/adapter/llmService/Gemini.py
+++ RLEnvForApp/adapter/llmService/Gemini.py
@@ -13,14 +13,26 @@ if "GOOGLE_API_KEY" not in os.environ:
 
 
 class Gemini(ILlmService):
+    MODEL_NAME_TO_MAX_OUTPUT_TOKENS_TABLE = {
+        "gemini-2.5-pro": 65536,
+        "gemini-2.5-flash": 65536,
+        "gemini-2.0-flash": 8192,
+    }
     llm = None
     structured_llm = None
 
-    def __init__(self, model_name="gemini-2.5-pro", temperature=0, structure_output_format=None):
-        self.llm = ChatGoogleGenerativeAI(
-            model=model_name,
-            temperature=temperature,
-        )
+    def __init__(self, model_name="gemini-2.5-pro", temperature=0, structure_output_format=None, thinking_budget=None, max_output_tokens=None, **kwargs):
+        gemini_param = dict()
+        gemini_param.update(kwargs)
+        # max_output_tokens
+        if max_output_tokens is None:
+            max_output_tokens = self.MODEL_NAME_TO_MAX_OUTPUT_TOKENS_TABLE.get(model_name)
+        if max_output_tokens is not None:
+            gemini_param["max_output_tokens"] = max_output_tokens
+        if thinking_budget is not None:
+            gemini_param["thinking_budget"] = thinking_budget
+
+        self.llm = ChatGoogleGenerativeAI(model=model_name, temperature=temperature, **gemini_param)
         if structure_output_format is not None:
             self.structured_llm = self.llm.with_structured_output(structure_output_format)
 
